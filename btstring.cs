73e56533bcb89786761b2d2589b50cf92227222f
anhnguyen3123 <anhnguyen.31231021117@st.ueh.edu.vn>
2024-08-19 16:25:29 +0700

    Add files via upload

---
+++ btstring.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class buoi3
+{
+    private static void Main(string[] args)
+    {
+        //EX1();
+        //EX2();
+        EX3();
+    }
+
+    static void EX1()
+    {
+        string x = Console.ReadLine();
+        Console.Write("The string yoou entered is: " + x);
+    }
+
+    static void EX2()
+    {
+        string x = Console.ReadLine();
+        Console.Write("Length of the string is: " + x.Length);
+    }
+
+    static void EX3()
+    {
+
+    }
+
